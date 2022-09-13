d10ccbdd7168851c78552b0cb6e4943f4c8af958
moonneer <108647944+moonneer@users.noreply.github.com>
2022-09-13 03:31:22 -0700

    Add files via upload

---
+++ Sudoku/sudoku.py
@@ -0,0 +1,79 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+def find_empty(bo):
+    for row in range(len(bo)):
+        for col in range(len(bo[0])):
+            if bo[row][col] == 0:
+                return (row, col) 
+    return None
+
+def valid(bo, guess, tup):
+    row_vals = bo[tup[0]]
+    if guess in row_vals:
+        return False
+
+    for i in range(len(bo)):
+        if bo[i][tup[1]] == guess and tup[0] != i:
+            return False
+
+    row_start = (tup[0] // 3) * 3
+    col_start = (tup[1] // 3) * 3
+
+    for i in range(row_start, row_start + 3):
+        for j in range(col_start, col_start + 3):
+            if bo[i][j] == guess and (i,j) != tup:
+                return False
+
+    return True
+
+
+
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+
+
+solve(board)
+print_board(board)
